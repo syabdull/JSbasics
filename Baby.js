840d73bd2ba10ca574352b83ba21da00dc90b03b
syabdull <syab5050@gmail.com>
2022-09-23 21:50:34 +0530

    prime prob added

--- Baby.js
+++ Baby.js
@@ -1,1 +1,32 @@
 console.log("Hello");
+ // variable is js
+     let a=10;
+     
+       // while loop
+
+        let ans=10;
+        
+        while(ans>0){
+            console.log(ans);
+            ans--;
+
+        }
+
+        //  check the given  no is prime or not 
+
+         let num=11;
+         let isPrime=true;
+
+        
+          for(let i=2;i*i<=num ; i++){
+             if(num%i==0){
+                     isPrime=false;
+             }
+
+          }
+
+         if(isPrime==true){
+            console.log("given no is prime");
+         }else{
+            console.log("given no is  not a prime");
+         }
